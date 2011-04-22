188906f0dbcee111c78a9a1755af504ad76a1e6c
Matt Todd <matt.c.todd@gmail.com>
2011-04-22 07:43:06 -0500

    actually implemented the batch job and the update job

--- reach_batch/app/batch_job.rb
+++ reach_batch/app/batch_job.rb
@@ -1,3 +1,10 @@
 require "reach_client"
+require "api_key_provider"
 
 puts "Running batch job..."
+client = ReachClient.new
+
+puts "latest games: "
+client.all_historic_games.each do |game|
+   puts "#{game.id}"
+end
