f0ce91232b9c7c51c45d764a3e33add7f71674a1
Matt Todd <matt.c.todd@gmail.com>
2011-05-01 05:53:01 -0500

    refactored client into a two step process: 1.) get game ids 2.) populate details

--- reach_batch/app/batch_job.rb
+++ reach_batch/app/batch_job.rb
@@ -5,11 +5,16 @@ require "active_record"
 
 puts "Running batch job..."
 
-client = ReachClient.new
 
 puts "latest games: "
-client.all_historic_games.each do |game|
-   puts "#{game.id}"
+all_games = ReachClient.new.all_historic_game_ids
+all_game_details = ReachClient.new.populate_details(all_games)
+statistics_extracter = StatisticsExtracter.new
+all_game_details.each do |game|
+   all_player_statistics = statistics_extracter.extract_statistics(game)
+   all_player_statistics.each do |player_statistics|
+      player_statistics.save
+   end
 end
 
 
