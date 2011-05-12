74e68f0c577e223af53a3078963ab5777f8a61b3
Matt Todd <matt.c.todd@gmail.com>
2011-05-12 07:15:24 -0500

    initial commit of the rails app

---
+++ reach_web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReachWeb::Application.config.secret_token = 'b0a39a9cafad5f97474ed6a73bb663b1cda57cdbccb42312844bc5c8bfd47e37498a0d44653f917a39d6baf43de28a1e9aacb994ab81dc5a9305cb753ebfd808'
